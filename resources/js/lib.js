d9baeaeb060e40d17fca11262b5fc60e54b084a9
Popchenko Matvey <m.popchenko@cft.ru>
2018-03-20 17:55:11 +0300

    Добавлены
    - иконки
    - файл с настройками json
    - css
    - меню
    - запуск консольных команд

--- resources/js/lib.js
+++ resources/js/lib.js
@@ -1,20 +1,20 @@
-function openCity(evt, cityName) {
-    // Declare all variables
-    var i, tabcontent, tablinks;
 
-    // Get all elements with class="tabcontent" and hide them
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
+  var winWidth=800; // ширина окна
+  var winHeight=600; // высота окна
+        // изменяем размер
+   window.resizeTo(winWidth, winHeight);
 
-    // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
+        // окно в центр экрана
+   var winPosX=screen.width/2-winWidth/2;
+   var winPosY=screen.height/2-winHeight/2;
+   window.moveTo(winPosX, winPosY);
 
-    // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(cityName).style.display = "block";
-    evt.currentTarget.className += " active";
-}
+$(document).ready(function() {
+      
+      $(".tablinks").click(function(){
+        $('.tablinks').removeClass('active')
+        $(this).addClass('active')
+        $('.tabcontent').hide()
+        $('#'+$(this).val()).show()
+      });
+});
